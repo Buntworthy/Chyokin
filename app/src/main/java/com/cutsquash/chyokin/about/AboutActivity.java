b4d16fe3c472bcc68f25eee14f87bd354cfe8dcd
Buntworthy <justinpinkney@gmail.com>
2016-02-13 16:27:48 +0000

    About screen finished

--- app/src/main/java/com/cutsquash/chyokin/about/AboutActivity.java
+++ app/src/main/java/com/cutsquash/chyokin/about/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.cutsquash.chyokin.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.cutsquash.chyokin.R;
 
@@ -11,5 +14,19 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void goGithub(View view) {
+        String url = getString(R.string.github_repo);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    public void goTwitter(View view) {
+        String url = getString(R.string.twitter);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
